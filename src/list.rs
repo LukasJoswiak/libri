053ceae79ef72b09cc701275dea45ca9a4f5aeb4
Lukas Joswiak <lukas@lukasjoswiak.com>
2022-01-01 13:44:27 -0800

    Abstract function to read book data from epubs

--- src/list.rs
+++ src/list.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::path::Path;
 
 use crate::common;
 use crate::config;
 use crate::format::epub;
 
-pub fn run(config: &config::Config) -> Result<(), Box<dyn Error>> {
+/// Returns a mapping of author to a vector of book titles. Reads ebook data from epubs in the given path.
+pub fn get_books(path: &Path) -> Result<HashMap<String, Vec<String>>, Box<dyn Error>> {
     let mut ebooks = HashMap::new();
-    let ebook_paths = common::find_books(&config.library)?;
+    let ebook_paths = common::find_books(&path)?;
 
-    // TODO: Abstract this part as well, also used in import
     for path in ebook_paths {
         let ebook = match epub::parse(path.as_path()) {
             Ok(ebook) => ebook,
@@ -19,11 +20,15 @@ pub fn run(config: &config::Config) -> Result<(), Box<dyn Error>> {
         let entry = ebooks.entry(ebook.author).or_insert(Vec::new());
         entry.push(ebook.title);
     }
+    Ok(ebooks)
+}
+
+pub fn run(config: &config::Config) -> Result<(), Box<dyn Error>> {
+    let ebooks = get_books(&config.library)?;
 
     // TODO: Sort alphabetically by author, then by title
     for (author, title) in ebooks {
         println!("{}\n  {}", author, title.join("\n  "));
     }
-
     Ok(())
 }
