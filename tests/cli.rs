d0ac8f9bc5749d63988588dd3cb2de6abf07ead3
Lukas Joswiak <lukas@lukasjoswiak.com>
2022-02-05 21:09:45 -0800

    Add unit test for invalid configuration directory

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,6 +5,21 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 
 #[test]
+fn config() -> Result<(), Box<dyn Error>> {
+    let dir = assert_fs::TempDir::new().unwrap();
+    let mut dir_path = dir.path().to_path_buf();
+    dir_path.push("missing/");
+
+    let mut cmd = Command::cargo_bin("libri")?;
+    cmd.arg("--config-dir").arg(dir_path).arg("list");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("assertion failed: dir.is_dir()"));
+
+    Ok(())
+}
+
+#[test]
 fn unknown_subcommand() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("libri")?;
 
@@ -40,3 +55,7 @@ fn list_unknown_argument() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+// TODO: Test all help variants
+// libri --help list // prints global help
+// libri list --help // prints list help
